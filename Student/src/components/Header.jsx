586f4c0e936b332f22a3187ec8a80c9f7bb406f3
spy-adi <83129751+spy-adi@users.noreply.github.com>
2021-12-03 11:04:20 +0530

    Added Logout button

--- Student/src/components/Header.jsx
+++ Student/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout} from 'antd';
+import { Layout, Button} from 'antd';
 import "../index.css";
 import "antd/dist/antd.css"
 
@@ -10,6 +10,9 @@ function Headerr(){
         <Header className="site-layout-background" style={{ padding: 0 }}>
           <span><img src="https://moodle.iitism.ac.in/pluginfile.php/1/core_admin/logo/0x150/1633851416/IIT_ISM_Logo.png" alt="Indian Institute of Technology (ISM) Dhanbad" style={{height:"65px",display:"inline",marginLeft:"20px"}}/>
           <h3 style={{color:"#396EB0",display:"inline"}}>Indian Institute of Technology (ISM), Dhanbad</h3></span>
+          <Button type="primary" danger style={{float: 'right', margin: '10px'}}>
+            Log Out
+          </Button>
         </Header>
     );
 }
