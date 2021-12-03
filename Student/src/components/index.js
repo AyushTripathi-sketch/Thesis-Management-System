9f1b046a0fbf03b8706f3e01e457a4313638d81f
spy-adi <83129751+spy-adi@users.noreply.github.com>
2021-12-03 10:57:51 +0530

    Added home in components

--- Student/src/components/index.js
+++ Student/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Headerr } from "./Header";
 export { default as Footerr } from "./Footer";
 export {default as SideBar } from "./SideBar";
 export { default as Profile } from "../pages/Profile";
+export { default as Home } from "../pages/Home";
