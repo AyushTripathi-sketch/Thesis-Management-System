8727c6fc45452377918b7611b279b4cdb65c6314
spy-adi <83129751+spy-adi@users.noreply.github.com>
2021-12-07 10:49:45 +0530

    Added My project components

--- Student/src/components/index.js
+++ Student/src/components/index.js
@@ -1,8 +1,13 @@
 export { default as Headerr } from "./Header";
 export { default as Footerr } from "./Footer";
 export {default as SideBar } from "./SideBar";
+export {default as MyProjectNav } from "./MyProjectNav";
 export { default as Profile } from "../pages/Profile";
 export { default as Home } from "../pages/Home";
 export { default as CourseWaiver } from "../pages/Course_Waiver";
 export { default as Courses } from "../pages/Courses";
-export { default as ProgressReport } from "../pages/Progress_Report"
+export { default as ProgressReport } from "../pages/Progress_Report";
+export { default as Overview } from "../pages/MyProject/Overview";
+export { default as Forums } from "../pages/MyProject/Forums";
+export { default as ActivityPlan } from "../pages/MyProject/ActivityPlan";
+export { default as Final } from "../pages/MyProject/Final";
