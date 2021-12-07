91229d7fda4d356f2f5bf9fafa87e5cf12902582
spy-adi <83129751+spy-adi@users.noreply.github.com>
2021-12-07 10:52:03 +0530

    Updated routes for my project

--- Student/src/index.js
+++ Student/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import { Layout} from 'antd'
-import { SideBar,Headerr,Footerr,Home,Profile,Courses, CourseWaiver, ProgressReport} from './components';
+import { SideBar,Headerr,Footerr,Home,Profile,Courses, CourseWaiver, ProgressReport,Overview, ActivityPlan, Forums, Final} from './components';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
@@ -21,6 +21,10 @@ class SiderDemo extends React.Component {
               <Route path='/profile' element={<Profile/>} />
               <Route path='/course-waiver' element={<CourseWaiver/>}/>
               <Route path='/progress-report' element={<ProgressReport/>}/>
+              <Route path='/myprojectOverview' element={<Overview />}/>
+              <Route path='/myprojectActivityPlan' element={<ActivityPlan />}/>
+              <Route path='/myprojectForums' element={<Forums />}/>
+              <Route path='/myprojectFinal' element={<Final />}/>
             </Routes>
             <Footerr />
           </Layout>
