cb54470a10826a28f9be35bc4a82e209d9bfa442
spy-adi <83129751+spy-adi@users.noreply.github.com>
2021-12-14 18:50:28 +0530

    Update Overview.jsx

--- src/portals/Student/pages/MyProject/Overview.jsx
+++ src/portals/Student/pages/MyProject/Overview.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Layout} from 'antd';
+import { Layout, Progress} from 'antd';
 import "antd/dist/antd.css";
-import { SideBar, Headerr, Footerr, MyProjectNav } from '../../components'
+import { SideBar, Headerr, Footerr, MyProjectNav, LineGraph } from '../../components'
 const {Content} = Layout;
 
 function Overview(){
@@ -12,7 +12,41 @@ function Overview(){
           <Headerr />
           <Content style={{ margin: "25px 25px" }}>
             <MyProjectNav />
-            <div></div>
+            <div className='row site-layout-background' style={{marginLeft:'0px', padding:'10px'}}>
+                <h3><u>Project Progress</u></h3>
+                <p>X Days remaining</p>
+                <Progress percent={50} status='active' />
+                <div style={{marginBottom:'10px'}}></div>
+              <div className='col-8 ' style={{padding:'10px'}}>
+                <LineGraph />
+              </div>
+              <div className='col-4'>
+                <div className='border' style={{marginBottom:'10px'}}>
+                <h6 style={{paddingLeft: '20px', background: '#002140', color: 'white'}}>Upcoming Events</h6>
+                  <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                </div>
+               
+                <div className='border' style={{marginBottom:'10px'}}>
+                <h6 style={{paddingLeft: '20px', background: '#002140', color: 'white'}}>Latest Events</h6>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                </div>
+                
+                <div className='border' style={{marginBottom:'10px'}}>
+                <h6 style={{paddingLeft: '20px', background: '#002140', color: 'white'}}>Final Seminar Grade</h6>
+                  XXXXXX XXXXXX XXXXXX XXXXXXXXXXXX
+                </div>
+              </div>
+            </div>
           </Content>
           <Footerr />
         </Layout>
