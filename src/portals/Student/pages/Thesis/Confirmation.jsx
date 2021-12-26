f9c7bcf455bfbe88a13a934941f30caea36102c0
Aditya <adi22feb@gmail.com>
2021-12-26 15:52:16 +0530

    Thesis review update

--- src/portals/Student/pages/Thesis/Confirmation.jsx
+++ src/portals/Student/pages/Thesis/Confirmation.jsx
@@ -10,62 +10,77 @@ function Confirmation() {
         className="site-layout-background"
         style={{ padding: "10px", height: "100%" }}
       >
-        <h2 style={{ paddingTop: "10px" }}>Review and Submit</h2>
+      <div className="text-center">
+      <h2>Thesis Submission</h2>
+      </div>
         <Divider />
-        <div className="container">
-          <div style={{ width: "100%", height: "auto", padding: "1%" }}>
-            <div>
-              <img
-                style={{
-                  marginRight: "20px",
-                  float: "left",
-                  width: "40%",
-                  maxWidth: "150px",
-                  height: "150 px",
-                }}
-                src={scholar}
-                alt="Scholar"
-              />
-            </div>
-            <div style={{ margin: "0 auto" }}>
-              <p>
-                <b>Scholar ID: </b>XXXXXX
-              </p>
-              <p>
-                <b>Name: </b>XXXXXX
-              </p>
-              <p>
-                <b>Branch: </b>XXXXXX
-              </p>
-              <p>
-                <b>Department: </b>XXXXXX
-              </p>
-              <p>
-                <b>Title: </b>XXXXXX
-              </p>
-              <p>
-                <b>Abstract: </b>XXXXXX
-              </p>
-              <p>
-                <b>Synopsis: </b>
-                <Button>Click to View</Button>
-              </p>
-              <p>
-                <b>Thesis: </b>
-                <Button>Click to View</Button>
-              </p>
+        <div className="student-profile py-4">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-4">
+                  <div
+                    className="card shadow-sm"
+                    style={{ marginBottom: "20px" }}
+                  >
+                    <div className="card-header bg-transparent text-center">
+                      <img
+                        className="profile_img"
+                        src={scholar}
+                        alt="student dp"
+                      />
+                      <h3>Name</h3>
+                    </div>
+                    <div className="card-body">
+                      <p className="mb-0">
+                        <strong className="pr-1">Scholar ID : </strong>19DRXXXX
+                      </p>
+                      <p className="mb-0">
+                        <strong className="pr-1">Department : </strong>XYZ
+                      </p>
+                      <p className="mb-0">
+                        <strong className="pr-1">Branch : </strong>XYZ
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-8">
+                  <div className="card shadow-sm">
+                    <div className="card-header bg-transparent border-0">
+                    </div>
+                    <div className="card-body pt-0">
+                      <table className="table table-bordered">
+                        <tbody>
+                          <tr>
+                            <th width="30%">Title</th>
+                            <td width="2%">:</td>
+                            <td>Lorem Ipsum</td>
+                          </tr>
+                          <tr>
+                            <th width="30%">Abstract</th>
+                            <td width="2%">:</td>
+                            <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</td>
+                          </tr>
+                          <tr>
+                            <th width="30%">Synopsis</th>
+                            <td width="2%">:</td>
+                            <td><Button>Click to View</Button></td>
+                          </tr>
+                          <tr>
+                            <th width="30%">Thesis</th>
+                            <td width="2%">:</td>
+                            <td><Button>Click to View</Button></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div style={{ float: "right", padding: "10px" }}>
-            <Button type="primary" htmlType="submit">
-              <a href="/st/thesis-submission">Edit</a>
-            </Button>
-            <span style={{ paddingRight: "20px" }} />
-            <Button type="primary" htmlType="submit">
-              <a href="/st/home">Submit</a>
-            </Button>
+          <div className="container text-center">
+            <Button type="primary">Submit</Button>
           </div>
-        </div>
       </div>
     </Content>
   );
