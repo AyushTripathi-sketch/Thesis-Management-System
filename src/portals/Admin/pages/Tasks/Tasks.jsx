afbc5568771599ee98130bab5924817cdbede2f5
spy-adi <83129751+spy-adi@users.noreply.github.com>
2021-12-28 21:29:52 +0530

    Update Tasks.jsx

--- src/portals/Admin/pages/Tasks/Tasks.jsx
+++ src/portals/Admin/pages/Tasks/Tasks.jsx
@@ -24,7 +24,7 @@ function Tasks() {
     }
   >
     <Meta
-      title="Upload Form"
+      description="Upload Form"
       style={{fontSize:'2vw !important'}}
     />
   </Card>
@@ -45,7 +45,7 @@ function Tasks() {
     }
   >
     <Meta
-      title="Comprehensive Examination Eligibility list"
+      description="Comprehensive Examination Eligibility list"
     />
   </Card>
   </Link>
@@ -65,7 +65,7 @@ function Tasks() {
     }
   >
     <Meta
-      title="Research Proposal Seminar Eligibility list"
+      description="Research Proposal Seminar Eligibility list"
     />
   </Card>
   </Link>
@@ -85,7 +85,27 @@ function Tasks() {
     }
   >
     <Meta
-      title="Pre-Submission Seminar Eligibility list"
+      description="Pre-Submission Seminar Eligibility list"
+    />
+  </Card>
+  </Link>
+  </Col>
+
+  <Col span={6}>
+  <Link to={'/ad/tasks/request_thesis_evaluation'}>
+  <Card
+      hoverable
+      className="rounded"
+    style={{ maxWidth: 200, textAlign: 'center', margin:"10px" }}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+  >
+    <Meta
+      description="Request for Thesis Evaluation"
     />
   </Card>
   </Link>
