c0082d99f3d02e8f14f2752845b8fbe36aa79252
spy-adi <83129751+spy-adi@users.noreply.github.com>
2022-01-01 18:51:12 +0530

    Update Examiner.js

--- src/portals/Examiner/Examiner.js
+++ src/portals/Examiner/Examiner.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Layout } from "antd";
-import { Headerr, Footerr } from "../../CommonComponents";
-import { Home } from "./components";
+import { Headerr, Footerr, Navbar, SideBar } from "../../CommonComponents";
+import { EvaluationForm, Home, HonorariumForm } from "./components";
 import { Routes, Route } from "react-router-dom";
+import menu from "./menu";
 
 function Examiner() {
   return (
     <>
+    <Navbar menu={menu} />
       <Layout style={{ minHeight: "100vh" }}>
+      <SideBar menu={menu} />
         <Layout className="site-layout">
           <Headerr />
           <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/eval" element={<EvaluationForm />} />
+          <Route path="/hon" element={<HonorariumForm />} />
           </Routes>
           <Footerr />
         </Layout>
