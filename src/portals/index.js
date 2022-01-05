a4bd8768be5385799f6f879e72707b8b4b5064fe
spy-adi <83129751+spy-adi@users.noreply.github.com>
2022-01-05 14:11:13 +0530

    Update index.js

--- src/portals/index.js
+++ src/portals/index.js
@@ -3,3 +3,4 @@ export { default as Student} from "./Student/Student";
 export { default as Supervisor} from "./Supervisor/Supervisor";
 export { default as Admin} from "./Admin/Admin";
 export { default as Examiner} from "./Examiner/Examiner";
+export { default as Consent} from "./Consent/Consent";
